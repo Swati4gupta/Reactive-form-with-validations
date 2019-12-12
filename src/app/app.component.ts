1f72ad80efa19c460b98834c403ea0da6ac82d7e
Swati4gupta <swati.gupta3900@gmail.com>
2019-12-12 12:39:50 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,58 @@
 import { Component,OnInit } from '@angular/core';
-import { FormControl,FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { ReactiveFormsModule,FormGroup,Validators,FormArray,FormControl,FormBuilder} from '@angular/forms';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements OnInit{
   form:FormGroup;
-  constructor(private formbuilder:FormBuilder){
+constructor(private formBuilder:FormBuilder){
 
-  }
-  ngOnInit(){
-   this.form= this.formbuilder.group({
-      name:['',Validators.required],
-      email:['',[Validators.required,Validators.email]]
-    })
+}
+ngOnInit(){
+  this.form=this.formBuilder.group({
+    name:['',Validators.required],
+    email:['',Validators.required],
+    address:this.formBuilder.array([])
+  })
+
+}
+  addAddressFields()
+  {
+    const addressForm=this.form.get('address') as FormArray;
+    addressForm.push(this.formBuilder.group({
+      addressLine1:[''],
+      addressLine2:[''] 
+      })
+    )
   }
 
-  isFieldValid(field){
-    return this.form.get(field).dirty && this.form.get(field).invalid;
+  removeAddressFields(){
+const addressForm = this.form.get('address') as FormArray;
+addressForm.removeAt(addressForm.length-1);
   }
+
+  submitForm(){
+    if(this.form.valid){
+      alert("form valid")
+    }else{
+     this.validateAllFields(this.form);
+    }
+  }
+
+  validateAllFields(form:FormGroup){
+     Object.keys(form.controls).forEach(field=>{
+       const control=form.get(field);
+        if(control instanceof FormControl){
+          control.markAsDirty({onlySelf:true});
+        }else if(control instanceof FormGroup){
+          this.validateAllFields(control);
+        }
+      })
+  }
+
   displayFieldCss(field){
     
     return {
@@ -29,22 +61,10 @@ export class AppComponent implements OnInit {
     }
   }
 
-onSubmit(){
- if(this.form.valid){
-      alert('form valid')
-    }else{
-      this.validateAllFields(this.form);
-    }
-}
-  validateAllFields(formgroup:FormGroup){
-      Object.keys(formgroup.controls).forEach(field=>{
-        const control=this.form.get(field); 
-        if(control instanceof FormControl){ 
-          control.markAsDirty({onlySelf:true});
-        }else if(control instanceof FormGroup){
-          this.validateAllFields(control);
-        }
-      })
-    }
+  isFieldValid(field){
+    return this.form.get(field).dirty && this.form.get(field).invalid;
+  }
+
+
   
 }
