95a5eaae2e123e83cbe3a131999be4b9ec208167
Swati4gupta <swati.gupta3900@gmail.com>
2019-12-12 16:39:55 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import {Routes,RouterModule} from '@angular/routes';
+import {Routes,RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 import { SibilingComponent } from './sibiling/sibiling.component';
 import { DataService } from './data.service';
 
-export const routes:Routes=[{path:'child',component:ChildComponent}];
+export const routes:Routes=[{path:'parent',component:AppComponent},
+{path:'child',component:ChildComponent},
+{path:'sibiling',component:SibilingComponent},
+{path:'',redirectTo:'parent',pathMatch:'full'}];
 @NgModule({
   imports:      [ BrowserModule, FormsModule,ReactiveFormsModule, 
   RouterModule.forRoot(routes) ],
